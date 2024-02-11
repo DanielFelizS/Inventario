a42f727ca0215602bb1e1285d806caf004b6b69d
DANIEL  FELIZ  SANTANA <a19a05048@educacion.edu.do>
2024-02-10 21:24:33 -0400

    Mejorando codigo

--- src/pages/EditarDatos.tsx
+++ src/pages/EditarDatos.tsx
@@ -1,14 +1,10 @@
-/* eslint-disable react/prop-types */
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FormEdit from "../molecules/Forms/FormEditar.jsx";
-import ObtenerDatos from "../components/ObtenerDatos";
 import { useNavigate } from "react-router-dom";
 
 export const EditarDispositivo = () => {
   const [error] = useState<string>("");
-
-  <ObtenerDatos />
 
   const navigate = useNavigate();
   const NavigateHome = () => {
